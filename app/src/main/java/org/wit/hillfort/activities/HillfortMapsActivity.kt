17e1011c24912a26c9ab929564e56a6def6c7881
sarahbarron <sarahflavin@yahoo.com>
2020-11-25 20:00:01 +0000

    layout for map of all hillforts

--- app/src/main/java/org/wit/hillfort/activities/HillfortMapsActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortMapsActivity.kt
@@ -2,16 +2,20 @@ package org.wit.hillfort.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
 import kotlinx.android.synthetic.main.activity_hillfort_list.*
+import kotlinx.android.synthetic.main.activity_hillfort_list.toolbar
+import kotlinx.android.synthetic.main.activity_hillfort_maps.*
 import org.wit.hillfort.R
 
 class HillfortMapsActivity : AppCompatActivity() {
+    lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort_maps)
-
-        // setup the toobar
-        setSupportActionBar(toolbar)
         toolbar.title = title
+        setSupportActionBar(toolbar)
+        mapView.onCreate(savedInstanceState);
     }
 }
