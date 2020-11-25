dda3243702e05c8034341bdea9bea869e34c3526
sarahbarron <sarahflavin@yahoo.com>
2020-11-25 20:01:40 +0000

    lifecycle events for the map added

--- app/src/main/java/org/wit/hillfort/activities/HillfortMapsActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortMapsActivity.kt
@@ -18,4 +18,29 @@ class HillfortMapsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         mapView.onCreate(savedInstanceState);
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
 }
