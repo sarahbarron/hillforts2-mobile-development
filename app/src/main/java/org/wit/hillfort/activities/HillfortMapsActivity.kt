1a1e221c82f5920469e836af2b07724dd821f836
sarahbarron <sarahflavin@yahoo.com>
2020-11-25 21:01:53 +0000

    layout of current map card with hillfort name and description

--- app/src/main/java/org/wit/hillfort/activities/HillfortMapsActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortMapsActivity.kt
@@ -2,21 +2,51 @@ package org.wit.hillfort.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_hillfort_list.*
 import kotlinx.android.synthetic.main.activity_hillfort_list.toolbar
 import kotlinx.android.synthetic.main.activity_hillfort_maps.*
+import org.jetbrains.anko.info
 import org.wit.hillfort.R
+import org.wit.hillfort.main.MainApp
+import org.wit.hillfort.models.UserModel
 
 class HillfortMapsActivity : AppCompatActivity() {
     lateinit var map: GoogleMap
+    lateinit var app: MainApp
+    var user = UserModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        app = application as MainApp
+
         setContentView(R.layout.activity_hillfort_maps)
         toolbar.title = title
         setSupportActionBar(toolbar)
         mapView.onCreate(savedInstanceState);
+
+        if(intent.hasExtra("user"))
+        {
+            user = intent.extras?.getParcelable<UserModel>("user")!!
+        }
+        // initialize the map object and then call the configuration method
+        mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
+    }
+
+    fun configureMap() {
+        map.uiSettings.setZoomControlsEnabled(true)
+        app.hillforts.findAll(user.id).forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.name).position(loc)
+            map.addMarker(options).tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
     }
 
     override fun onDestroy() {
